4495dec0368ee9024e9f4ddd7b89241172fa63bf
Massolinii <massimilianoesposito548@gmail.com>
2023-10-25 17:31:04 +0200

    Chi-siamo page finished

--- src/components/Contatti.js
+++ src/components/Contatti.js
@@ -1,27 +1,48 @@
+import SVGIcons from "@/assets/SvgIcons";
+
 export default function Contatti() {
   return (
     <div className="bg-black w-full pb-20">
-      <div className="container mx-auto p-2">
-        <div className="border-b border-l p-2 grid grid-cols-1 md:grid-cols-3 gap-4  text-center">
-          <div className="bg-magenta border-2 border-amber-400 h-[150px] flex flex-col items-center justify-center">
-            <h3 className="text-lg mb-2">TELEFONO</h3>
-            <p>
-              +39 123 456 789 <a href="tel:+39123456789">📞</a>{" "}
-              <a href="https://wa.me/+39123456789">💬</a>
-            </p>
-            <p>
-              +39 987 654 321 <a href="tel:+39987654321">📞</a>{" "}
-              <a href="https://wa.me/+39987654321">💬</a>
-            </p>
+      <div className="container mx-auto p-2 ">
+        <div className="border-b border-l p-2 grid grid-cols-1 md:grid-cols-3 gap-2 text-center">
+          <div className="bg-magenta border border-stone-300 h-[200px] flex flex-col items-center justify-center">
+            <h3 className="text-lg mb-2 text-3xl">TELEFONO</h3>
+
+            <div className="flex items-center my-3">
+              <a href="tel:+393450913688">
+                <SVGIcons.PhoneBig />
+              </a>
+              <span className="ml-2">Telefona al 345 0913688</span>
+            </div>
+
+            <div className="flex items-center">
+              <a href="https://wa.me/+393891253407">
+                <SVGIcons.Whatsapp />
+              </a>
+              <span className="ml-2">Scrivici al 389 1253407</span>
+            </div>
           </div>
-          <div className="bg-magenta border-2 border-amber-400 h-[150px] flex flex-col items-center justify-center">
-            <h3 className="text-lg mb-2">E-MAIL</h3>
-            <p>brughibarsas@gmail.com</p>
+          <div className="bg-magenta border border-stone-300 h-[200px] flex flex-col items-center justify-center">
+            <h3 className="text-lg mb-2 text-3xl">E-MAIL</h3>
+            <a
+              href="mailto:brughibarsas@gmail.com"
+              className="no-underline text-white "
+            >
+              brughibarsas@gmail.com
+            </a>
           </div>
-          <div className="bg-magenta border-2 border-amber-400 h-[150px] flex flex-col items-center justify-center pt-3">
-            <h3 className="text-lg mb-2">ORARI</h3>
-            <p>Dal Lunedì al Venerdì : 07:00-23:00</p>
-            <p>Sabato e Domenica : 08:30-12:00 / 15:00-23:00</p>
+          <div className="bg-magenta border border-stone-300 h-[200px] flex flex-col items-center justify-center pt-3">
+            <h3 className="text-lg mb-2 text-3xl">ORARI</h3>
+            <p className="mb-2">
+              <span className="font-bold">Dal Lunedì al Venerdì:</span>
+              <br />
+              07:00-23:00
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Sabato e Domenica:</span>
+              <br />
+              08:30-12:00 / 15:00-23:00
+            </p>
             <p className="underline font-bold">Mercoledì chiuso!</p>
           </div>
         </div>
